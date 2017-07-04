961ee4426309dd1491f3221088b4802186fb000f
psclil <psclil@gmail.com>
2017-07-04 12:34:02 +0300

    Implemented main statistics added string formatting to main statistics display

--- knesset-app/StatisticsWindow.xaml.cs
+++ knesset-app/StatisticsWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using knesset_app.DBEntities;
+using System;
+using System.Linq;
+using System.Windows;
 
 namespace knesset_app
 {
@@ -14,7 +17,42 @@ namespace knesset_app
 
         private void InitStatistics(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                var res = new GeneralStatisticsResult();
+                using (var context = new KnessetContext())
+                {
+                    context.Database.Log = Console.WriteLine;
+                    res.NumProtocols = context.Protocols.Count();
+                    int numParagraphs = context.Paragraphs.Count();
+                    res.ParagraphsPerProtocol = (float)numParagraphs / res.NumProtocols;
+                    /*
+                     * calculate speakers by protocol
+                     * the key is that each speaker has only one paragraph with pn_ph_number=1
+                     SELECT AVG(sq.c) as v FROM (
+	                    SELECT COUNT(*) as c
+	                    FROM paragraph p
+	                    WHERE p.pn_pg_number = 1
+	                    GROUP BY p.c_name, p.pr_number
+                    ) as sq
+                     */
+                    res.SpeakersPerProtocol = (float)context.Paragraphs
+                                                        .Where(x => x.pn_pg_number == 1)
+                                                        .GroupBy(x => x.protocol)
+                                                        .Select(r => r.Count())
+                                                        .Average();
+                    res.ParagraphsPerProtocolSpeaker = res.ParagraphsPerProtocol / res.SpeakersPerProtocol;
+                    int numWordsSpoken = context.ParagraphWords.Count();
+                    res.WordsPerProtocol = (float)numWordsSpoken / res.NumProtocols;
+                    res.WordsPerParagraph = (float)numWordsSpoken / numParagraphs;
+                }
+                DataContext = res;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                Close();
+            }
         }
 
         private void WordFrequencies(object sender, RoutedEventArgs e)
