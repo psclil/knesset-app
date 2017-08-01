d3160479cec2b1cac67c8206a4d653e93d375dd7
Adam <adamsen@outlook.com>
2017-08-01 09:53:15 +0300

    הוספה והסרה של ביטוי. צריך ליטוש נוסף

--- knesset-app/SearchWindow.xaml.cs
+++ knesset-app/SearchWindow.xaml.cs
@@ -7,7 +7,6 @@ using knesset_app.DBEntities;
 using System.Data.Entity;
 using System.Text.RegularExpressions;
 using System.Windows.Input;
-using System.Windows.Media;
 
 namespace knesset_app
 {
@@ -25,72 +24,25 @@ namespace knesset_app
         public SearchWindow()
         {
             InitializeComponent();
-            PopulateComboboxes();
-            dpListExpression.PreviewMouseRightButtonDown += OnPreviewMouseRightButtonDown;
+            PopulateCommitteeComboboxe();
+            PopulatePhraseComboboxe();
         }
 
 
 
-        private void PopulateComboboxes()
+        private void PopulateCommitteeComboboxe()
         {
             cbProtocolCommitte.ItemsSource = context.Committees.Take(MAX_COMBOXLIST).ToList();
             cbProtocolCommitte.DisplayMemberPath = "c_name";
             cbProtocolCommitte.SelectedValuePath = "c_name";
 
+        }
+
+        private void PopulatePhraseComboboxe()
+        {
             dpListExpression.ItemsSource = context.Phrases.Take(MAX_COMBOXLIST).ToList();
             dpListExpression.DisplayMemberPath = "phrase";
             dpListExpression.SelectedValuePath = "phrase";
-        }
-
-
-
-
-        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            // ... Get TabControl reference.
-            TabControl item = sender as TabControl;
-            // ... Set Title to selected tab header.
-            var selected = item.SelectedItem as TabItem;
-            this.Title = selected.Header.ToString();
-            TabItem ti = (TabItem)item.SelectedItem;
-
-            if (ti.Name == "tiSearchCommitties")
-            {
-
-            }
-        }
-
-
-
-
-
-        private void btnClearFields_Click(object sender, RoutedEventArgs e)
-        {
-
-            // Committee Tab
-            cbProtocolTitle.Text = string.Empty;
-            cbProtocolCommitte.Text = string.Empty;
-            cbProtocolCommitte.Text = string.Empty;
-            cbInvited.Text = null;
-            cbPersence.Text = null;
-            dpFromDate.SelectedDate = dpToDate.SelectedDate = null;
-            lstResults.ItemsSource = string.Empty;
-
-            // Backward Tab
-            protocolName.Text = string.Empty;
-            PgOffset.Text = string.Empty;
-            SpkeakerName.Text = string.Empty;
-            PgOffset.Text = null;
-            PgSpeakerNum.Text = null;
-            paragraphNum.Text = null;
-            wordNum.Text = null;
-            lstResultsBackwardSearch.ItemsSource = string.Empty;
-
-            // Expression Tab
-            dpListExpression.Text = string.Empty;
-            dpListExpression.SelectedIndex = -1;
-            lsResultsExpression.ItemsSource = string.Empty;
-
         }
 
 
@@ -231,7 +183,6 @@ namespace knesset_app
                 int paragraphSpekerOffset = int.Parse(PgOffset.Text);
 
 
-
                 IQueryable<Protocol> relevantProtocols = context.Protocols;
                 IQueryable<Paragraph> relevantParagraphs = context.Paragraphs;
 
@@ -324,6 +275,57 @@ namespace knesset_app
 
 
 
+        private void openChosenProtocol(object sender, SelectionChangedEventArgs e)
+        {
+            if (lstResults.SelectedIndex == -1) return;
+            ProtocolDisplayWindow chosenP = new ProtocolDisplayWindow(lstResults.SelectedItem as Protocol);
+            chosenP.ShowDialog();
+            lstResults.SelectedIndex = -1;
+        }
+
+
+        private void openChosenProtocolExpression(object sender, SelectionChangedEventArgs e)
+        {
+            if (lstResults.SelectedIndex == -1) return;
+            ProtocolDisplayWindow chosenP = new ProtocolDisplayWindow(lstResults.SelectedItem as Protocol);
+            chosenP.ShowDialog();
+            lstResults.SelectedIndex = -1;
+        }
+
+
+
+        private void btnClearFields_Click(object sender, RoutedEventArgs e)
+        {
+
+            // Committee Tab
+            cbProtocolTitle.Text = string.Empty;
+            cbProtocolCommitte.Text = string.Empty;
+            cbProtocolCommitte.Text = string.Empty;
+            cbInvited.Text = null;
+            cbPersence.Text = null;
+            dpFromDate.SelectedDate = dpToDate.SelectedDate = null;
+            lstResults.ItemsSource = string.Empty;
+
+            // Backward Tab
+            protocolName.Text = string.Empty;
+            PgOffset.Text = string.Empty;
+            SpkeakerName.Text = string.Empty;
+            PgOffset.Text = null;
+            PgSpeakerNum.Text = null;
+            paragraphNum.Text = null;
+            wordNum.Text = null;
+            lstResultsBackwardSearch.ItemsSource = string.Empty;
+
+            // Expression Tab
+            dpListExpression.Text = string.Empty;
+            dpListExpression.SelectedIndex = -1;
+            lsResultsExpression.ItemsSource = string.Empty;
+
+        }
+
+
+
+
         private void numericTxt_Change(object sender, EventArgs e)
         {
             if (sender == null || !(sender is TextBox)) return;
@@ -334,14 +336,6 @@ namespace knesset_app
             }
         }
 
-
-        private void openChosenProtocol(object sender, SelectionChangedEventArgs e)
-        {
-            if (lstResults.SelectedIndex == -1) return;
-            ProtocolDisplayWindow chosenP = new ProtocolDisplayWindow(lstResults.SelectedItem as Protocol);
-            chosenP.ShowDialog();
-            lstResults.SelectedIndex = -1;
-        }
 
 
 
@@ -365,61 +359,6 @@ namespace knesset_app
 
 
 
-
-
-
-
-
-
-
-
-        //private void MenuItem_OnClick(object sender, RoutedEventArgs e)
-        //{
-        //    var menuItem = (MenuItem)sender;
-        //    var ctxMenu = (ContextMenu)menuItem.Parent;
-        //    var comboBoxItem = (ComboBoxItem)ctxMenu.PlacementTarget;
-        //}
-
-
-
-
-
-        private void OnPreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            var comboBoxItem = VisualUpwardSearch(e.OriginalSource as DependencyObject);
-
-            if (comboBoxItem == null) return;
-            comboBoxItem.IsSelected = true;
-            e.Handled = true;
-        }
-
-        private ComboBoxItem VisualUpwardSearch(DependencyObject source)
-        {
-            while (source != null && !(source is ComboBoxItem))
-                source = VisualTreeHelper.GetParent(source);
-
-            return source as ComboBoxItem;
-        }
-
-        private void MenuItem_OnClick(object sender, RoutedEventArgs e)
-        {
-            dpListExpression.Items.Remove(dpListExpression.SelectedItem);
-        }
-
-
-        private void dpListExpression_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            //if (dpListExpression.SelectedIndex >= 0)
-            //{
-
-            //    dpListExpression.Items.RemoveAt(dpListExpression.SelectedIndex);
-            //    return;
-            //}
-
-        }
-
-
-
         private void AddExpreesion(object sender, RoutedEventArgs e)
         {
             string expressionToAdd = string.IsNullOrEmpty(dpListExpression.Text) ? string.Empty : dpListExpression.Text.Trim();
@@ -431,20 +370,16 @@ namespace knesset_app
             }
             try
             {
-                using (KnessetContext context = new KnessetContext())
+                Phrase existing = context.Phrases.Find(expressionToAdd);
+                if (existing != null)
                 {
-                    Phrase existing = context.Phrases.Find(expressionToAdd);
-                    if (existing != null)
-                    {
-                        MessageBox.Show("כבר קיים ביטוי כזה");
-                        return;
-                    }
-                    Phrase reader = new Phrase { phrase = expressionToAdd };
-                    context.Phrases.Add(reader);
-                    context.SaveChanges();
+                    MessageBox.Show("כבר קיים ביטוי כזה");
+                    return;
                 }
-                DialogResult = true;
-                Close();
+                Phrase reader = new Phrase { phrase = expressionToAdd };
+                context.Phrases.Add(reader);
+                context.SaveChanges();
+                PopulatePhraseComboboxe();
             }
             catch (Exception ex)
             {
@@ -454,18 +389,62 @@ namespace knesset_app
 
 
 
-        private void DeleteGroup(object sender, RoutedEventArgs e)
+        private void DeletePhrase(object sender, RoutedEventArgs e)
         {
 
-            string expressionToRemove = dpListExpression.SelectedItem.ToString();
-            using (KnessetContext context = new KnessetContext())
+            string expressionToRemove = dpListExpression.Text;
             {
                 Phrase reader = context.Phrases.Find(expressionToRemove);
                 context.Phrases.Remove(reader);
                 context.SaveChanges();
-                //   Phrase reader = dpListExpression.SelectedValuePath;
-                //    context.Phrases.Attach(dpListExpression.SelectedItem);
-                //    context.Phrases.Remove(x=> w.phrase == expressionToRemove);
+                dpListExpression.Text = string.Empty;
+                PopulatePhraseComboboxe();
+
+            }
+        }
+
+
+
+        private Boolean isPhraseExists(string phrase)
+        {
+            Boolean found = false;
+            foreach (Phrase Item in dpListExpression.Items)
+            {
+                if (Item.phrase == phrase)
+                {
+                    return found = true;
+                }
+            }
+            return found;
+        }
+
+
+
+        private void dpListExpression_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            onSearch();
+
+        }
+
+
+        private void dpListExpression_KeyUp(object sender, KeyEventArgs e)
+        {
+            onSearch();
+        }
+
+        private void onSearch()
+        {
+            string phrase = dpListExpression.Text;
+            if (isPhraseExists(phrase))
+            {
+                btnAdd.IsEnabled = false;
+                btnRemove.IsEnabled = true;
+            }
+            else
+            {
+                btnAdd.IsEnabled = true;
+                btnRemove.IsEnabled = false;
+
             }
         }
 
